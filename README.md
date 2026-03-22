8b2a393263307a2fe00f4ed7a2a0d773a773ab9b
jordan15citizen <jordanrodrigues168@gmail.com>
2026-03-22 10:24:04 +0000

    initial commit: add root directories

---
+++ README.md
@@ -0,0 +1,2 @@
+# gitmas
+Gitmas is multi-tool for git and other things
---
+++ src/gitmas.nim
@@ -0,0 +1,118 @@
+import std/os
+import std/osproc
+import std/strutils
+import std/terminal
+
+const YLW = ansiStyleCode(styleBright) & ansiForegroundColorCode(fgYellow)
+const RST = ansiResetCode
+const RED = ansiStyleCode(styleBright) & ansiForegroundColorCode(fgRed)
+
+proc sys_prop_get(key: cstring, value: cstring): int32 
+  {.importc: "__system_property_get", header: "<sys/system_properties.h>".}
+
+proc getAndroidProp(key: string): string =
+  var buffer = newString(1024) 
+  let length = sys_prop_get(key.cstring, buffer.cstring)
+  if length > 0:
+    return $buffer.cstring
+  else:
+    return "Not found"
+
+proc gitPush(commitMsg: string) =
+  echo "- Committing..."
+  discard execCmd("git add .")
+  discard execCmd("git commit -am " & quoteShell(commitMsg))
+  echo "- Pushing..."
+  discard execCmd("git push origin main")
+
+proc gitInit(addStruc: bool) =
+  echo "- Initializing git..."
+  discard execCmd("git init")
+  discard execCmd("git branch -m main")
+  if addStruc:
+    echo "- Adding file structure..."
+    discard execCmd("git add .")
+  else:
+    echo "- Not adding file structure."
+
+proc showHelp() =
+  echo "- Gitmas by Jordan"
+  echo "- Do not type grinch, or else..."
+  echo ""
+  
+  echo "Commands:"
+  echo ""
+
+  echo "init <true|false>:"
+  echo "- Initialize git and add file structure based on input."
+       
+  echo "push <commitMsg>:"
+  echo "- Push files to remote."
+
+  echo "sys-info:"
+  echo "- Get system information."
+  echo ""
+
+proc showSystemInfo() =
+  let androidVer =   getAndroidProp("ro.build.version.release")
+  let androidSDK = getAndroidProp("ro.build.version.sdk")
+  let deviceModel = getAndroidProp("ro.product.model")
+  let sysABI = getAndroidProp("ro.product.cpu.abi")
+  echo "Android Version: " & androidVer
+  echo "Android SDK: " & androidSDK
+  echo "Device Model: " & deviceModel
+  echo "System ABI: " & sysABI
+
+proc warn(msgWarn: string) =
+  echo YLW & "warning: " & RST & msgWarn
+
+proc error(msgErr: string) =
+  echo RED & "error: " & RST & msgErr
+
+let args = commandLineParams()
+
+if args.len == 0:
+  warn "no command given."
+  warn"redirecting to help."
+  echo ""
+  showHelp()
+  quit(0)
+
+if args.len > 0:
+  let command = args[0]
+
+  case command
+    of "sys-info":
+      showSystemInfo()
+      
+    of "init":
+      if args.len > 1:
+        let shouldAdd = parseBool(args[1]) 
+        gitInit(shouldAdd)
+      else:
+        error "'init' needs a 'true' or 'false' argument."
+      
+    of "push":
+      if args.len > 1:
+        let argMsg = args[1]
+        if args[1].isEmptyOrWhitespace():
+          error "commit message cannot be empty or whitespace!"
+        else:
+          gitPush(argMsg)
+      else:
+        error "commit message not given!"  
+
+    of "help":
+     if args.len >= 1:
+       showHelp()
+
+    of "grinch":
+      error "YOU DARE TRY IT?"
+      error "Yeah you did, so now nothing will happen."
+      echo "- Grinch go kaboom!"
+      quit(1)
+       
+    else:
+    
+      error "invalid command " & command
+      echo "Try 'init', 'push' or 'help'."
